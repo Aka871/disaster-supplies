0c2c2e9ebb93e25168c3305d82f433e7d6667262
Aka <163530203+Aka871@users.noreply.github.com>
2026-05-20 20:32:27 +0900

    init: Spring Initializrでプロジェクト作成

---
+++ src/main/java/com/aka/disastersupplies/DisastersuppliesApplication.java
@@ -0,0 +1,13 @@
+package com.aka.disastersupplies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisastersuppliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisastersuppliesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aka/disastersupplies/DisastersuppliesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aka.disastersupplies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisastersuppliesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
